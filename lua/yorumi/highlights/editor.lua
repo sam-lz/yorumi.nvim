ff4de23e7febfc34bbd046ba0aecd419898483f3
Samuel Lozmann <lozmannsamuel@gmail.com>
2025-11-10 21:44:59 +0100

    v0.6?

--- lua/yorumi/highlights/editor.lua
+++ lua/yorumi/highlights/editor.lua
@@ -19,7 +19,7 @@ function M.setup(config)
     DiffDelete    = { bg = palette.kuroiRed, fg = palette.kairoRed },
     DiffText      = { bg = palette.kairoYellow, fg = palette.yoru0 },
     -- EndOfBuffer   = { fg = palette.yoru2 },
-    EndOfBuffer   = { fg = palette.tsuki1 },
+    EndOfBuffer   = { fg = palette.yoru3 },
     ErrorMsg      = { fg = palette.kairoRed },
     WinSeparator  = { fg = palette.yoru3 },
     VertSplit     = { link = "WinSeparator" },
